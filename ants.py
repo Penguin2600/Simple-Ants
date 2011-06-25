4cb0803b6c0f0bb366028c2854fa1f5a47cb1ace
Charles Bock <cbock@asu.edu>
2011-06-24 19:29:00 -0700

    Unfucked tabs

--- ants.py
+++ ants.py
@@ -4,21 +4,20 @@ from math import *
 
 class Colony(list):
     def __init__(self,c, n, x, y, w, h, r):
-        
         self.food = 10
         self.color=c
         self.step=0
         self.maxstep=7
         for i in range(n):
-                self.append(Ant(self, x, y, self.nstep()))
-                self.step+=1
-                
+            self.append(Ant(self, x, y, self.nstep()))
+            self.step+=1
+        
         self.x = x      #X position
         self.y = y      #Y position
         self.w = w      #Screen Width
         self.h = h      #Screen Height
         self.r = r      #Max wander radius
-        
+    
     def nstep(self):    #keep track of steps
         self.step+=1
         if (self.step > self.maxstep): self.step=0
@@ -30,21 +29,21 @@ class Colony(list):
 
 class Food:
     def __init__(self, x, y, size):
-        self.x = x              #X position
-        self.y = y              #Y position
-        self.size = size        #Size of food blob
+        self.x = x          #X position
+        self.y = y          #Y position
+        self.size = size    #Size of food blob
 
     def draw(self, surface):    #Draw our foods
-            pygame.draw.circle(surface, [0,150,0], (int(self.x), int(self.y)), int(self.size))
+        pygame.draw.circle(surface, [0,150,0], (int(self.x), int(self.y)), int(self.size))
 
 class Pheromone:
     
     def __init__(self, x, y, r):
-        self.x = x              #X position
-        self.y = y              #Y position
-        self.r = r              #Heading
+        self.x = x        #X position
+        self.y = y        #Y position
+        self.r = r        #Heading
         self.strength = 1.0     #Streangth of signal
-        
+    
     def update(self, d=0.005):  #Degrade over time
         self.strength -= d
 
@@ -60,7 +59,7 @@ class Ant:
     def __init__(self, colony, x, y, step):
     
         self.colony = colony
-    
+        
         self.x = x                              #X position
         self.y = y                              #Y position
         self.r = int(random.random()*359)       #Initial heading
@@ -76,7 +75,7 @@ class Ant:
         if d < radius:
             return True
         return False
-        
+    
     def heading(self, obj): #Set ant's heading twoards a destination
    
         dx = (obj.x-self.x) 
@@ -88,12 +87,12 @@ class Ant:
         self.wandering = 0
 
     
-    def wander(self, d=5.0):                                           #Ants must wander to find food.
+    def wander(self, d=5.0):                       #Ants must wander to find food.
 
         self.wandering+=1
-        self.r+=((random.random() * d*2)-(d))                           #Add random value to heading
+        self.r+=((random.random() * d*2)-(d))              #Add random value to heading
 
-        if self.x > self.colony.w: self.heading(self.colony)            #Dont leave the screen
+        if self.x > self.colony.w: self.heading(self.colony)        #Dont leave the screen
         if self.x < 0: self.heading(self.colony)
         if self.y > self.colony.h: self.heading(self.colony)
         if self.y < 0: self.heading(self.colony)
@@ -105,37 +104,37 @@ class Ant:
 
         if self.has_food == False:      #If we dont have food and we find a trail, follow it!
             for pheromone in trails:
-                if self.near(pheromone, 20):
+                if self.near(pheromone, 30):
                     if random.random() < pheromone.strength: #Random chance to disregard the trail
                         self.r=pheromone.r
     
     def harvest(self, foodsource):
-        
-        for food in foodsource:         #Get food if we find it
+    
+        for food in foodsource:  #Get food if we find it
             if self.near(food, radius=food.size+2) and self.has_food == False: 
                 food.size -= 1
                 self.has_food = True
-        
+    
     def recall(self, trails):
-        
+    
         if self.has_food:
-                self.heading(self.colony)
-                if (len(trails)==0):
-                        trails.append(Pheromone(self.x, self.y, (self.r-180)))
-                else:
-                        placetrail=1
-                        for pheromone in trails:
-                                if self.near(pheromone, 20):
-                                        pheromone.strength=1.0
-                                        placetrail=0
-                        if (placetrail):
-                                trails.append(Pheromone(self.x, self.y, (self.r-180)))
+            self.heading(self.colony)
+            if (len(trails)==0):
+                trails.append(Pheromone(self.x, self.y, (self.r-180)))
+            else:
+                placetrail=1
+                for pheromone in trails:
+                    if self.near(pheromone, 30):
+                        pheromone.strength=1.0
+                        placetrail=0
+                if (placetrail):
+                    trails.append(Pheromone(self.x, self.y, (self.r-180)))
         
-                        if self.near(self.colony): #Drop food and start wandering again
-                            self.has_food = False
-                            self.r+=180
-                            self.colony.food += 1
-                            self.colony.append(Ant(self.colony, self.colony.x, self.colony.y, self.colony.nstep()))
+                if self.near(self.colony): #Drop food and start wandering again
+                    self.has_food = False
+                    self.r+=180
+                    self.colony.food += 1
+                    self.colony.append(Ant(self.colony, self.colony.x, self.colony.y, self.colony.nstep()))
 
     def fight(self, colonies):
         for colony in colonies:
@@ -143,35 +142,35 @@ class Ant:
                 for ant in colony:      
                     if self.near(ant): 
                         if (random.random() > .5):
-                                self.colony.remove(self)
+                            self.colony.remove(self)
                         else:
-                                colony.remove(ant)
+                            colony.remove(ant)
     
     def update(self, trails, foodsource, colonies):
 
-        if (self.step)==0:               #
-                self.follow(trails)     #follow nearby trails to food.
-                
-        self.harvest(foodsource)        #harvest nearby food source
+        if (self.step)==0:         
+            self.follow(trails)     #follow nearby trails to food.
+            
+        self.harvest(foodsource)    #harvest nearby food source
         
         if (self.step)==4:  
-                self.recall(trails)             #bring food directly to colony
+            self.recall(trails)     #bring food directly to colony
         
-        self.wander()                   #some random wandering is more efficient
-        self.fight(colonies)           #FIGHT
+        self.wander()               #some random wandering is more efficient
+        self.fight(colonies)        #FIGHT
 
         self.y += self.s*sin(radians(self.r))   #Move!
         self.x += self.s*cos(radians(self.r)) 
 
         self.step+=1
         if (self.step > self.colony.maxstep): self.step=0
-        
+    
     def draw(self, surface):    #Draw our ant
         y=int(self.y+2*sin(radians(self.r)))
         x=int(self.x+2*cos(radians(self.r)))
+        
         pygame.draw.line(surface, self.colony.color, (int(self.x), int(self.y)),(x,y), 2)
-
-
+        
 class Text:
 
     def __init__(self, color, size, pos, text):
@@ -181,6 +180,6 @@ class Text:
         self.rect.topleft=pos
 
     def draw(self, surface):
-                surface.blit(self.image,self.rect.topleft)
-        
+        surface.blit(self.image,self.rect.topleft)
+    
 
--- antsrun.py
+++ antsrun.py
@@ -1,95 +1,95 @@
 import ants, time, pygame, random
 
 
-def setup():                            #Setup all our lists and establish two ant colonies (RED vs BLU)
-        global colonies, trails, foods, text, debug, showtrail
-        colonies = []
-        trails = []
-        foods = []
-        text = []
-        colonies.append(ants.Colony([0,0,150],10, 150, 150, WIDTH, HEIGHT, 300))
-        colonies.append(ants.Colony([150,0,0],10, 650, 650, WIDTH, HEIGHT, 300))
-        debug=0
-        showtrail=0
+def setup():                #Setup all our lists and establish two ant colonies (RED vs BLU)
+    global colonies, trails, foods, text, debug, showtrail
+    colonies = []
+    trails = []
+    foods = []
+    text = []
+    colonies.append(ants.Colony([0,0,150],50, 150, 150, WIDTH, HEIGHT, 300))
+    colonies.append(ants.Colony([150,0,0],50, 650, 650, WIDTH, HEIGHT, 300))
+    debug=0
+    showtrail=0
 
-def doinput():                          #Check for input ESC for exit d for debug mode.
-        global debug, showtrail
-        for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                        keys[event.key] = True
-                elif event.type == pygame.KEYUP:
-                        keys[event.key] = False
-                if keys[pygame.K_ESCAPE]:
-                        exit(0)
-                if keys[pygame.K_d]:
-                        debug+=1
-                if (debug>1): debug=0
-                if keys[pygame.K_t]:
-                        showtrail+=1
-                if (showtrail>1): showtrail=0
-                        
-def update():                           #Update all our ants, trails, and foods.
-        global colonies, trails, foods, text, debug, showtrail
-        
-        MainSurf.fill([157,107,64])     #blank our background with a pleasant brown.
+def doinput():            #Check for input ESC for exit d for debug mode.
+    global debug, showtrail
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            keys[event.key] = True
+        elif event.type == pygame.KEYUP:
+            keys[event.key] = False
+        if keys[pygame.K_ESCAPE]:
+            exit(0)
+        if keys[pygame.K_d]:
+            debug+=1
+        if (debug>1): debug=0
+        if keys[pygame.K_t]:
+            showtrail+=1
+        if (showtrail>1): showtrail=0
 
-        if len(trails) > 0:
-                for pheromone in trails:                #If there are any trails we need to degrade them over time
-                        pheromone.update()
-                        if (showtrail==1):
-                                pheromone.draw(MainSurf)
-                        if pheromone.strength < 0.05:   #Trails that grow weak should be removed.
-                                trails.remove(pheromone)
+def update():              #Update all our ants, trails, and foods.
+    global colonies, trails, foods, text, debug, showtrail
 
-        
-        for f in foods:                 #Update and draw our food blobs.
-                f.draw(MainSurf)
-                if (f.size <2): foods.remove(f)
-                
-        if (len(foods) < 1):    #Place new ones if the current ones are mined out.
-                for i in range(2):
-                        x = 100 + random.random()*(WIDTH-100)
-                        y = 100 + random.random()*(HEIGHT-100)
-                        s = (random.random()*20)+20
-                        foods.append(ants.Food(x,y,s)) 
-        
-        text=[]                         #text method only used for debug mode at this point.
+    MainSurf.fill([157,107,64])     #blank our background with a pleasant brown.
 
-        for colony in colonies:         #for each colony we need to go through and update and draw the ants.
-                colony.draw(MainSurf)
-                text.append(ants.Text([150,150,150], 10, (colony.x-5, colony.y+30), str(len(colony))))
-                for ant in colony:
-                        ant.update(trails,foods,colonies)
-                        ant.draw(MainSurf)
-                
-                        if debug==1:    #if debug mode is on draw our ants heading and position.
-                                text.append(ants.Text([150,150,150], 10, (ant.x, ant.y), str((int(ant.step),int(ant.r)))))
-                
-        
-        for t in text:                  #draw any text we have.
-                t.draw(MainSurf)
-            
-        pygame.display.update()         #draw our new frame.
+    if len(trails) > 0:
+        for pheromone in trails:        #If there are any trails we need to degrade them over time
+            pheromone.update()
+            if (showtrail==1):
+                pheromone.draw(MainSurf)
+            if pheromone.strength < 0.05:   #Trails that grow weak should be removed.
+                trails.remove(pheromone)
+
+
+    for f in foods:      #Update and draw our food blobs.
+        f.draw(MainSurf)
+        if (f.size <2): foods.remove(f)
+
+    if (len(foods) < 1):    #Place new ones if the current ones are mined out.
+        for i in range(2):
+            x = 100 + random.random()*(WIDTH-100)
+            y = 100 + random.random()*(HEIGHT-100)
+            s = (random.random()*20)+20
+            foods.append(ants.Food(x,y,s))
+
+    text=[]          #text method only used for debug mode at this point.
+
+    for colony in colonies:  #for each colony we need to go through and update and draw the ants.
+        colony.draw(MainSurf)
+        text.append(ants.Text([150,150,150], 10, (colony.x-5, colony.y+30), str(len(colony))))
+        for ant in colony:
+            ant.update(trails,foods,colonies)
+            ant.draw(MainSurf)
+
+            if debug==1:    #if debug mode is on draw our ants heading and position.
+                text.append(ants.Text([150,150,150], 10, (ant.x, ant.y), str((int(ant.step),int(ant.r)))))
+
+
+    for t in text:        #draw any text we have.
+        t.draw(MainSurf)
+
+    pygame.display.update()  #draw our new frame.
 
 
 if __name__ == "__main__":
 
-        
-        pygame.init()                   #initialize pygame
-        clock = pygame.time.Clock()     #start our FPS clock
 
-        #global constants here
-        WIDTH=800
-        HEIGHT=800
-        MainSurf = pygame.display.set_mode([WIDTH, HEIGHT])
+    pygame.init()          #initialize pygame
+    clock = pygame.time.Clock()     #start our FPS clock
 
-        keys = {pygame.K_ESCAPE : False, pygame.K_d : False, pygame.K_t : False} #Declare keys we will use
-        
-        setup()                                 #run our init function
-        
-        while 1:                                #main loop
-                clock.tick(500)                  #Limit FPS to 30
-                doinput()                       #Do input functions
-                update()                        #Do main update routene
+    #global constants here
+    WIDTH=800
+    HEIGHT=800
+    MainSurf = pygame.display.set_mode([WIDTH, HEIGHT])
 
-        exit(0)
+    keys = {pygame.K_ESCAPE : False, pygame.K_d : False, pygame.K_t : False} #Declare keys we will use
+
+    setup()              #run our init function
+
+    while 1:                #main loop
+        clock.tick(30)      #Limit FPS to 30
+        doinput()           #Do input functions
+        update()            #Do main update routene
+
+    exit(0)
